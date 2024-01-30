fc320129855f6cc15e2592407c92b2d2bc34c030
PayRoom <water38198@gmail.com>
2024-01-30 10:31:54 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,141 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+const peopleBox = ref([]);
+const people = ["Ataraxia", "carrie 🍄", "CPing", "hsuhsusophie", "Ivy", "jeremyChan", "jia yu", "PayRoom", "WA", "銀光菇"];
+const groupNum = ref(0);
+const waitArea = ref([]);
+const colors = ref({});
+const allBoxes = ref([]);
+const animatePerson = ref([]);
+// 把拖動目標資料抓出來儲存
+function dragged(e) {
+  e.dataTransfer.setData("text/plain", e.target.textContent)
+}
+// 在等待分組區域新增該 person
+function drop(e) {
+  cancelDefault(e);
+  // 放下的地方錯誤的話，不作用
+  if (e.target.id !== "waitArea") return
+  const name = e.dataTransfer.getData('text/plain');
+  if (waitArea.value.includes(name)) return
+  waitArea.value.push(name)
+  // e.target.appendChild(peopleBox.value.find(person => person.textContent === name))
+}
+// 取消預設行為
+function cancelDefault(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  return false
+}
+// 給每個人一個隨機的背景色
+function randomColor() {
+  const r = Math.floor((Math.random() * 255));
+  const g = Math.floor((Math.random() * 255));
+  const b = Math.floor((Math.random() * 255));
+  const color = `rgba(${r},${g},${b},0.5)`
+  return color;
+}
+function divideGroup() {
+  let length = waitArea.value.length
+  if (groupNum.value == 0 || !length) return
+  let box = 0;
+  while (length > 0) {
+    const random = Math.floor(Math.random() * length);
+    const el = waitArea.value[random];
+    console.log(el)
+    allBoxes.value[box % groupNum.value].push(el)
+    cancelPick(el)
+    // 補上動畫的 class
+    setTimeout(() => {
+      animatePerson.value.push(el);
+    }, box * 500)
+    box++;
+    length--;
+  }
+}
+function cancelPick(person) {
+  const index = waitArea.value.indexOf(person);
+  if (index === -1) return
+  waitArea.value.splice(index, 1)
+}
+function reset() {
+  waitArea.value = []
+  allBoxes.value = []
+  animatePerson.value = []
+  groupNum.value = 0
+}
+function defineGroupsNum() {
+  const num = groupNum.value;
+  allBoxes.value = [];
+  for (let i = 0; i < num; i++) {
+    allBoxes.value.push([]);
+  }
+  console.log(allBoxes.value)
+}
+onMounted(() => {
+  people.forEach((person) => {
+    colors.value[person] = randomColor()
+  })
+})
+</script>
+<template>
+  <div class="container mx-auto">
+    <h1>分組選擇器</h1>
+    <p>使用方法：將要分組的人拖曳進等待分組區，選擇好組數後，按下分組按鈕即可。</p>
+    <p>※點擊可以取消</p>
+    <h2>請選擇要分組的人員:</h2>
+    <div class="flex gap-4">
+      <template v-if="Object.keys(colors).length">
+        <div ref="peopleBox" class="person p-4 flex rd hover:cursor-pointer border border-solid h-50px items-center"
+          draggable="true" @dragstart="dragged" v-for="person in people" :key="person"
+          :style="{ 'background-color': colors[person] }">
+          {{ person }}
+        </div>
+      </template>
+    </div>
+    <h3>等待分組區域</h3>
+    <div class="flex gap-4 w-80% px-10 h-200px py-20 border border-solid border-black" @drop="drop"
+      @dragenter="cancelDefault" @dragover="cancelDefault" id="waitArea">
+      <div v-for="person in waitArea"
+        class="person p-4 flex rd hover:cursor-pointer border border-solid h-50px items-center"
+        :style="{ 'background-color': colors[person] }" @click="cancelPick(person)">{{ person }}</div>
+    </div>
+    <div class="py-4 flex items-center">
+      <label for="groupNum" class="me-4">要分幾組:</label>
+      <select name="groupNum" id="" class="w-100px font-size-5 p-2" v-model="groupNum" @change="defineGroupsNum()">
+        <option v-for="i in 5" :value="i" class="font-size-5">{{ i }}</option>
+      </select>
+    </div>
+    <div>
+      <button type="button"
+        class="px-10 py-4 hover:cursor-pointer me-4 rd border border-solid bg-transparent hover:bg-gray hover:text-white"
+        @click="divideGroup()">分組</button>
+      <button type="button"
+        class="px-4 py-2 hover:cursor-pointer me-4 rd border border-solid border-red bg-transparent color-red hover:bg-red hover:text-white"
+        @click="reset()">清除</button>
+    </div>
+    <div class="flex justify-center">
+      <div v-for="(num, index) in groupNum" :key="index" ref="allBoxDom" class="groups flex flex-col gap-4 me-10">
+        <h3 class="text-center">分組{{ num }}</h3>
+        <div v-for="person in allBoxes[index]"
+          class="person p-4 flex rd hover:cursor-pointer border border-solid h-50px items-center"
+          :style="{ 'background-color': colors[person] }" :class="{ 'animate': animatePerson.includes(person) }">{{ person
+          }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+* {
+  box-sizing: border-box;
+}
+
+.groups .person {
+  transform: scale(0);
+  transition: 0.5s;
+
+  &.animate {
+    transform: scale(1);
+  }
+}</style>
